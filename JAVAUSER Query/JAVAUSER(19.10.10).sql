924f87a3660651b40156164a189e3ecc4c86a826
Dong-jun Sin <tlsehdwns239@gmail.com>
2019-10-10 17:46:58 +0900

    19/10/10
    <자바-DB 연동>
    SUBJECT 테이블 사용

---
+++ JAVAUSER Query/JAVAUSER(19.10.10).sql
@@ -0,0 +1,31 @@
+--자바연동 books 테이블
+CREATE SEQUENCE books_seq
+START WITH 1
+INCREMENT BY 1
+MINVALUE 0
+MAXVALUE 9999
+NOCYCLE
+CACHE 2;
+
+DROP SEQUENCE books_seq;
+
+SELECT * FROM books;
+
+
+--자바연동 SUBJECT 테이블
+CREATE SEQUENCE subject_seq
+START WITH 1
+INCREMENT BY 1
+MINVALUE 1
+MAXVALUE 9999
+NOCYCLE
+CACHE 2;
+
+DROP SEQUENCE subject_seq;
+
+INSERT INTO subject(no, s_num, s_name) VALUES(subject_seq.NEXTVAL, '10', '통계학과');
+
+DELETE FROM subject WHERE no = '10';
+SELECT subject_seq.NEXTVAL FROM subject;
+
+SELECT * FROM subject;
