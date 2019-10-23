64973747b06b5e119b1a9f783ea49c9dc2622801
Dong-jun Sin <tlsehdwns239@gmail.com>
2019-10-23 17:47:16 +0900

    19/10/23
    Java, JavaFX, DB 연동을 활용한 예제(6~9)(수강관리),
    SubjectTabController 구현

---
+++ Oracle SQL/JAVAUSER(19.10.23).sql
@@ -0,0 +1,20 @@
+--파일 이름만 담아두는 용도의 컬럼(Filename) 생성
+ALTER TABLE subject ADD(filename varchar2(100));
+
+----자동으로 학과번호 부여 (표현방식별 쿼리)
+--s_num으로부터 0을 제거하고(TRIM) 가장 큰 값(MAX)에서 +1을 해준 후, 
+--전체 2자리의 구성 중 빈 자리를 0으로 채운다(LPAD) 만약 값이 없다면 01을 출력한다.
+SELECT NVL(LPAD(MAX(TRIM('0' FROM s_num))+1, 2, '0'), '01') AS subjectNum FROM subject;
+--시퀀스를 이용하여 4자리 코드번호 부여 (예를 들어 : 0005)
+SELECT TRIM(TO_CHAR(subject_seq.NEXTVAL, '0000')) AS codeNumber FROM dual;
+--문자와 시퀀스를 이용하여 문자 4자리 코드번호 부여(예를 들어: CODE0005)
+SELECT CONCAT('CODE', TRIM(TO_CHAR(subject_seq.NEXTVAL, '0000'))) AS codeNumber FROM dual;
+
+CREATE OR REPLACE SEQUENCE subject_seq IS
+START WITH 0
+INCREMENT BY 1
+MINVALUE 0
+MAXVALUE 100
+
+
+SELECT * FROM subject;
