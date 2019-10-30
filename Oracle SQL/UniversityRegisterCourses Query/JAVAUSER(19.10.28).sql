5c124f74929ad55c9e78e949f3f64d98f646c335
Dong-jun Sin <tlsehdwns239@gmail.com>
2019-10-30 17:50:17 +0900

    19/10/30
    프로시저를 이용한 무결성 제어 삭제 예제

--- Oracle SQL/UniversityRegisterCourses Query/JAVAUSER(19.10.28).sql
+++ Oracle SQL/UniversityRegisterCourses Query/JAVAUSER(19.10.28).sql
@@ -1,5 +1,42 @@
+/*
+ㅇ학과 정보를 삭제할 프로시저(subject_del)를 생성하여 보자.
+- 매개변수는 삭제할 대상의 no(vno) /IN, 
+  검증할 학과 번호(vs_num) /IN, 
+  결과를 반환할 메시지(vmsg) /OUT
+
+- 학과에 소속된 학생이 존재하지 않으면 삭제 후 '학과 정보를 정상적으로 삭제하였습니다.'
+
+- 학과에 소속된 학생이 존재하면 '학과에 소속된 학생이 존재하기 때문에 삭제할 수가 없습니다.'
+*/
+--예외처리를 활용
 CREATE OR REPLACE PROCEDURE subject_del(
-    vno IN subject.no%TYPE,
+    vno subject.no%TYPE, --IN은 기본값이라 생략 가능
+    vs_num IN subject.s_num%TYPE,
+    vmsg OUT VARCHAR2
+)
+IS
+   cnt NUMBER := 0;
+   DATAEXIST EXCEPTION; --예외 정의
+BEGIN
+        SELECT COUNT(*) INTO cnt FROM student WHERE s_num = vs_num;
+        
+    IF cnt = 0 THEN
+        DELETE FROM subject WHERE no = vno;
+        vmsg := '학과 정보를 정상적으로 삭제하였습니다.';
+        COMMIT; --변경사항을 작업 단위 저장
+    ELSE
+        RAISE DATAEXIST; --예외 발생
+    END IF;
+EXCEPTION 
+    WHEN DATAEXIST THEN
+        vmsg := '학과에 소속된 학생이 존재하기 때문에 삭제할 수가 없습니다.';
+    ROLLBACK; --변경사항을 작업 단위 취소
+END subject_del;
+/
+
+-- IF 활용
+CREATE OR REPLACE PROCEDURE subject_del(
+    vno subject.no%TYPE, --IN은 기본값이라 생략 가능
     vs_num IN subject.s_num%TYPE,
     vmsg OUT VARCHAR2
 )
@@ -11,16 +48,17 @@ BEGIN
     IF cnt = 0 THEN
         DELETE FROM subject WHERE no = vno;
         vmsg := '학과 정보를 정상적으로 삭제하였습니다.';
+        COMMIT; --변경사항을 작업 단위 저장
     ELSE
-        vmsg := '학과에 소속된 학생이 존재하기 때문에 삭제할 수가 없습니다.';
+       vmsg := '학과에 소속된 학생이 존재하기 때문에 삭제할 수가 없습니다.';
     END IF;
-END;
+END subject_del;
 /
 
 DECLARE
     msg VARCHAR2(100);
 BEGIN
-    subject_del(1, 01, msg);
+    subject_del(1, '01', msg); --자료형 맞춰서 대입하기
     DBMS_OUTPUT.PUT_LINE(msg);
 END;
 /
