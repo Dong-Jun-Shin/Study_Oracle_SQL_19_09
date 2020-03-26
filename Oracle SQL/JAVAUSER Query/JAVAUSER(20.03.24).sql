8bec62b0ca977e7603a1ebaed04b36aef8a2fec7
Dongjun-Shin <tlsehdwns239@gmail.com>
2020-03-26 14:36:03 +0900

    20/03/24
    Hint와 Index - 개념 및 활용한 페이징 처리

---
+++ Oracle SQL/JAVAUSER Query/JAVAUSER(20.03.24).sql
@@ -0,0 +1,51 @@
+-- ORDER BY를 활용한 정렬 조회
+SELECT * FROM spring_reply ORDER BY b_num desc;
+SELECT * FROM spring_board ORDER BY b_num desc;
+
+-- hint 문법과 INDEX를 활용한 정렬 조회
+SELECT /*+INDEX_DESC(spring_board spring_board_pk)*/
+    *
+FROM spring_board WHERE b_num > 0;
+
+
+-- rowid 고유 식별자
+SELECT rowid, b_num FROM spring_board;
+
+
+-- 힌트 없이 조회 (아무 정렬 기준 없는 상태로 순차 출력)
+SELECT rownum, b_num, b_name, b_title, to_char(b_date, 'YYYY-MM-DD') as b_date
+FROM spring_board WHERE rownum <= 10;
+
+-- 힌트에 제시한 기준으로 정렬 후, 1번부터 10번까지의 한 페이지 단위 데이터를 출력
+SELECT /*+ INDEX_DESC(spring_board spring_board_pk) */
+	rownum, b_num, b_name, b_title, to_char(b_date, 'YYYY-MM-DD') as b_date
+FROM spring_board WHERE rownum <= 10;
+
+-- In-line View로 힌트 활용한 데이터를 가져온 후, 11번부터 20번까지 출력 ()
+-- In-line View에서 끝번호 조건, 바깥 쿼리에서 시작번호 조건
+SELECT 
+    rnum, b_num, b_name, b_title, to_char(b_date, 'YYYY-MM-DD') as b_date
+FROM(
+    SELECT /*+ INDEX_DESC(spring_board spring_board_pk) */
+        rownum as rnum, b_num, b_name, b_title, b_date
+    FROM spring_board WHERE rownum <= 10 AND b_title LIKE '%08%'
+    )boardlist
+WHERE rnum > 0;
+
+
+--페이징 처리를 위한 샘플 데이터 입력 처리
+CREATE OR REPLACE PROCEDURE board_paging_insert
+IS
+BEGIN
+    FOR cnt IN 1..120 LOOP
+        INSERT INTO spring_board(b_num, b_name, b_title, b_content, b_pwd) 
+        VALUES(spring_board_seq.NEXTVAL, '홍길동'||cnt, 
+        '페이징 처리를 위한 데이터'||cnt, '페이징 처리를 위한 데이터 입력입니다', '1234');
+    END LOOP;
+    COMMIT;
+END;
+/
+SHOW ERROR;
+
+EXECUTE board_paging_insert;
+SELECT COUNT(*) FROM spring_board;
